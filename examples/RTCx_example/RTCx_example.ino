c4144078f3fde714e45f5d302facea12b8062958
Steve Marple <stevemarple@googlemail.com>
2018-05-09 15:55:08 +0100

    Reformat and update to use new autoprobe and print functions

--- examples/RTCx_example/RTCx_example.ino
+++ examples/RTCx_example/RTCx_example.ino
@@ -32,75 +32,45 @@
 #include <Wire.h>
 #include <RTCx.h>
 
-void printTm(Stream &str, struct RTCx::tm *tm)
-{
-  str.print(tm->tm_year + 1900);
-  str.print('-');
-  str.print(tm->tm_mon + 1);
-  str.print('-');
-  str.print(tm->tm_mday);
-  str.print('T');
-  str.print(tm->tm_hour);
-  str.print(':');
-  str.print(tm->tm_min);
-  str.print(':');
-  str.print(tm->tm_sec);
-  str.print(" yday=");
-  str.print(tm->tm_yday);
-  str.print(" wday=");
-  str.println(tm->tm_wday);
-}
-
 void setup(void)
 {
-  Serial.begin(9600);
-  Wire.begin();
-  // The address used by the DS1307 is also used by other devices (eg
-  // MCP3424 ADC). Test for a MCP7941x device first.
-  uint8_t addressList[] = {RTCx::MCP7941xAddress,
-			   RTCx::DS1307Address};
+	if (F_CPU > 8000000UL)
+		Serial.begin(115200);
+	else
+		Serial.begin(9600);
+	Wire.begin();
 
-  // Autoprobe to find a real-time clock.
-  if (rtc.autoprobe(addressList, sizeof(addressList))) {
-    // Found something, hopefully a clock.
-    Serial.print("Autoprobe found ");
-    switch (rtc.getDevice()) {
-      case RTCx::DS1307:
-	Serial.print("DS1307");
-	break;
-    case RTCx::MCP7941x:
-      Serial.print("MCP7941x");
-      break;
-    default:
-      // Ooops. Must update this example!
-      Serial.print("unknown device");
-      break;
-    }
-    Serial.print(" at 0x");
-    Serial.println(rtc.getAddress(), HEX);
-  }
-  else {
-    // Nothing found at any of the addresses listed.
-    Serial.println("No RTCx found");
-    return;
-  }
+	// Autoprobe to find a real-time clock.
+	if (rtc.autoprobe()) {
+		// Found something, hopefully a clock.
+		Serial.print("Autoprobe found ");
+		Serial.print(rtc.getDeviceName());
+		Serial.print(" at 0x");
+		Serial.println(rtc.getAddress(), HEX);
+	}
+	else {
+		// Nothing found at any of the addresses listed.
+		Serial.println("No RTCx found, cannot continue");
+		while (1)
+			;
+	}
 
-  // Enable the battery backup. This happens by default on the DS1307
-  // but needs to be enabled on the MCP7941x.
-  rtc.enableBatteryBackup();
+	// Enable the battery backup. This happens by default on the DS1307
+	// but needs to be enabled on the MCP7941x.
+	rtc.enableBatteryBackup();
 
-  // rtc.clearVBAT();
-  
-  // Ensure the oscillator is running.
-  rtc.startClock();
+	// rtc.clearVBAT();
 
-  if (rtc.getDevice() == RTCx::MCP7941x) {
-    Serial.print("Calibration: ");
-    Serial.println(rtc.getCalibration(), DEC);
-    // rtc.setCalibration(-127);
-  }
+	// Ensure the oscillator is running.
+	rtc.startClock();
 
-  rtc.setSQW(RTCx::freq4096Hz);
+	if (rtc.getDevice() == RTCx::MCP7941x) {
+		Serial.print("Calibration: ");
+		Serial.println(rtc.getCalibration(), DEC);
+		// rtc.setCalibration(-127);
+	}
+
+	rtc.setSQW(RTCx::freq4096Hz);
 }
 
 
@@ -110,86 +80,86 @@ uint8_t bufPos = 0;
 unsigned long last = 0;
 void loop(void)
 {
-  struct RTCx::tm tm;
-  if (millis() - last > 2000) {
-    last = millis();
-    rtc.readClock(tm);
-    
-    printTm(Serial, &tm);
-    RTCx::time_t t = RTCx::mktime(&tm);
-    printTm(Serial, &tm);
-    Serial.print("unixtime = ");
-    Serial.println(t);
-    Serial.println("-----");
-  }
+	struct RTCx::tm tm;
+	if (millis() - last > 2000) {
+		last = millis();
+		rtc.readClock(tm);
 
-  while (Serial.available()) {
-    char c = Serial.read();
-    if ((c == '\r' || c == '\n' || c == '\0')) {
-      if (bufPos <= bufLen && buffer[0] == 'C') {
-	// Check time error
-	buffer[bufPos] = '\0';
-	RTCx::time_t pcTime = atol(&(buffer[1]));
-	rtc.readClock(&tm);
-	RTCx::time_t mcuTime = RTCx::mktime(&tm);
-	Serial.print("MCU clock error: ");
-	Serial.print(mcuTime - pcTime);
-	Serial.println(" s");
-	Serial.println("~~~~~");
-      }
-      if (bufPos <= bufLen && buffer[0] == 'T') {
-	// Set time
-	buffer[bufPos] = '\0';
-	RTCx::time_t t = atol(&(buffer[1])); 
-	RTCx::gmtime_r(&t, &tm);
-	rtc.setClock(&tm);
-	Serial.println("Clock set");
-	Serial.println(&(buffer[0]));
-	printTm(Serial, &tm);
-	Serial.println("~~~~~");
-      }
-      if (bufPos <= bufLen && buffer[0] == 'X') {
-	// Set calibration value
-	buffer[bufPos] = '\0';
-	if (rtc.getDevice() == RTCx::MCP7941x) {
-	  int8_t oldCal = rtc.getCalibration();
-	  char *endptr;
-	  long cal = strtol(&(buffer[1]), &endptr, 0);
-	  if (cal >= -127 && cal <= 127 && endptr == &buffer[bufPos]) {
-	    Serial.print("Previous calibration: ");
-	    Serial.println(oldCal, DEC);
-	    Serial.print("Calibration: ");
-	    Serial.println(cal, DEC);
-	      rtc.setCalibration(cal);
-	  }
-	  else 
-	    Serial.println("Bad value for calibration");
+		RTCx::printIsotime(Serial, tm).println();
+		RTCx::time_t t = RTCx::mktime(&tm);
+
+		Serial.print("unixtime = ");
+		Serial.println(t);
+		Serial.println("-----");
 	}
-	else {
-	  Serial.println("Cannot set calibration: not a MCP7941x");
+
+	while (Serial.available()) {
+		char c = Serial.read();
+		if ((c == '\r' || c == '\n' || c == '\0')) {
+			if (bufPos <= bufLen && buffer[0] == 'C') {
+				// Check time error
+				buffer[bufPos] = '\0';
+				RTCx::time_t pcTime = atol(&(buffer[1]));
+				rtc.readClock(&tm);
+				RTCx::time_t mcuTime = RTCx::mktime(&tm);
+				Serial.print("MCU clock error: ");
+				Serial.print(mcuTime - pcTime);
+				Serial.println(" s");
+				Serial.println("~~~~~");
+			}
+			if (bufPos <= bufLen && buffer[0] == 'T') {
+				// Set time
+				buffer[bufPos] = '\0';
+				RTCx::time_t t = atol(&(buffer[1]));
+				RTCx::gmtime_r(&t, &tm);
+				rtc.setClock(&tm);
+				Serial.println("Clock set");
+				Serial.println(&(buffer[0]));
+				RTCx::printIsotime(Serial, tm);
+				Serial.println("~~~~~");
+			}
+			if (bufPos <= bufLen && buffer[0] == 'X') {
+				// Set calibration value
+				buffer[bufPos] = '\0';
+				if (rtc.getDevice() == RTCx::MCP7941x) {
+					int8_t oldCal = rtc.getCalibration();
+					char *endptr;
+					long cal = strtol(&(buffer[1]), &endptr, 0);
+					if (cal >= -127 && cal <= 127 && endptr == &buffer[bufPos]) {
+						Serial.print("Previous calibration: ");
+						Serial.println(oldCal, DEC);
+						Serial.print("Calibration: ");
+						Serial.println(cal, DEC);
+						rtc.setCalibration(cal);
+					}
+					else
+						Serial.println("Bad value for calibration");
+				}
+				else {
+					Serial.println("Cannot set calibration: not a MCP7941x");
+				}
+			}
+			if (bufPos <= bufLen && buffer[0] == 'M') {
+				// Set SQW mode
+				buffer[bufPos] = '\0';
+				char *endptr;
+				long mode = strtol(&(buffer[1]), &endptr, 0);
+				if (mode >= RTCx::freq1Hz && mode <= RTCx::freqCalibration
+					&& endptr == &buffer[bufPos]) {
+					if (rtc.setSQW((RTCx::freq_t)mode)) {
+						Serial.print("SQW: ");
+						Serial.println(mode, DEC);
+					}
+					else
+						Serial.println("Could not set SQW");
+				}
+				else
+					Serial.println("Bad value for SQW");
+			}
+			bufPos = 0;
+		}
+		else if (bufPos < bufLen)
+			// Store character
+			buffer[bufPos++] = c;
 	}
-      }
-      if (bufPos <= bufLen && buffer[0] == 'M') {
-	// Set SQW mode
-	buffer[bufPos] = '\0';
-	  char *endptr;
-	  long mode = strtol(&(buffer[1]), &endptr, 0);
-	  if (mode >= RTCx::freq1Hz && mode <= RTCx::freqCalibration
-	      && endptr == &buffer[bufPos]) {
-	    if (rtc.setSQW((RTCx::freq_t)mode)) { 
-	      Serial.print("SQW: ");
-	      Serial.println(mode, DEC);
-	    }
-	    else
-	      Serial.println("Could not set SQW");
-	  }
-	  else 
-	    Serial.println("Bad value for SQW");
-      }
-      bufPos = 0;
-    }
-    else if (bufPos < bufLen)
-      // Store character
-      buffer[bufPos++] = c; 
-  }
 }
